69c287e387c3757422e3dbb91b46f61a61743b54
Alex Wright <AlexParadiseWright+git@gmail.com>
2014-03-03 20:58:18 +0200

    Project Creation

---
+++ AndroidNetworkFramework/gen/za/ac/sun/ml/androidnetworkframework/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package za.ac.sun.ml.androidnetworkframework;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
